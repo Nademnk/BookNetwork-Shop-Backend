2d62a19508453414e4a5e643255a0bcc43b8bf52
Nadeem Khan <nadeemishtiaque1536@gmail.com>
2024-12-07 12:19:34 +0530

    Changed

---
+++ src/main/java/com/northstar/book_network_shop/history/BookTransactionHistory.java
@@ -0,0 +1,27 @@
+package com.northstar.book_network_shop.history;
+
+import com.northstar.book_network_shop.common.CommonClass;
+import com.northstar.book_network_shop.feedback.Feedback;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookTransactionHistory extends CommonClass{
+
+	//user relationship
+	//book relationship
+	
+	private boolean returned;
+	 private boolean returnApproved;
+	
+}
