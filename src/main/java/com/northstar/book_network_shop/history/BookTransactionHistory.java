742ebc48525c9cfbbe8d983eb767e2f740169142
Nadeem Khan <nadeemishtiaque1536@gmail.com>
2024-12-07 12:47:36 +0530

    Changed

--- src/main/java/com/northstar/book_network_shop/history/BookTransactionHistory.java
+++ src/main/java/com/northstar/book_network_shop/history/BookTransactionHistory.java
@@ -1,8 +1,12 @@
 package com.northstar.book_network_shop.history;
 
+import com.northstar.book_network_shop.book.Book;
 import com.northstar.book_network_shop.common.CommonClass;
+import com.northstar.book_network_shop.user.User;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,10 +21,15 @@ import lombok.experimental.SuperBuilder;
 @NoArgsConstructor
 public class BookTransactionHistory extends CommonClass{
 
-	//user relationship
-	//book relationship
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "boo_id")
+	private Book book; 
 	
 	private boolean returned;
-	 private boolean returnApproved;
+	private boolean returnApproved;
 	
 }
