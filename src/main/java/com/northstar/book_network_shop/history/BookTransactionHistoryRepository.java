bd66a2248ea8eff42873fe6da6d2ea1e37820399
Nadeem Khan <nadeemishtiaque1536@gmail.com>
2024-12-09 13:19:08 +0530

    Changed

--- src/main/java/com/northstar/book_network_shop/history/BookTransactionHistoryRepository.java
+++ src/main/java/com/northstar/book_network_shop/history/BookTransactionHistoryRepository.java
@@ -1,5 +1,7 @@
 package com.northstar.book_network_shop.history;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -25,6 +27,30 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
 			WHERE history.book.owner.id = :userId
 			
 			""")
-	Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Integer userId);
+	
+ Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Integer userId);
+
+ 
+ @Query("""
+         SELECT
+         (COUNT (*) > 0) AS isBorrowed
+         FROM BookTransactionHistory bookTransactionHistory
+         WHERE bookTransactionHistory.user.id = :userId
+         AND bookTransactionHistory.book.id = :bookId
+         AND bookTransactionHistory.returnApproved = false
+         """)
+
+boolean isAlreadyBorrowedByUser(Integer bookId, Integer userId);
+
+
+ @Query("""
+         SELECT transaction
+         FROM BookTransactionHistory  transaction
+         WHERE transaction.user.id = :userId
+         AND transaction.book.id = :bookId
+         AND transaction.returned = false
+         AND transaction.returnApproved = false
+         """)
+Optional<BookTransactionHistory> findByBookIdAndUserId(Integer bookId, Integer  userId);
 
 }
